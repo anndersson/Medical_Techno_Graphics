1214f80c773bcdf29056494da924d1c0fd388383
davidnatareno <ednatareno@scl.edu.gt>
2022-09-23 10:17:07 -0600

    Video

--- js/proteina.js
+++ js/proteina.js
@@ -23,19 +23,11 @@ el cuerpo humano no es capaz de utilizar las proteínas ingeridas mediante la al
 <video controls autoplay>
 <source src="img/VID-20220923-WA0013.mp4" type="video/mp4">
 </video>
-<<<<<<< HEAD
-
-=======
->>>>>>> 5ade89ca6e07d5412eb0e6dccf9f63df2093969c
 
 <h1 class="Alimentos"> Proteinas Por Alimentos </h1>
 <div class="prot">
 <img class="alimentos2" src="img/inasprote.jpg">
 <img class="alimentos3" src="img/inas.jpg">
-<<<<<<< HEAD
-
-=======
->>>>>>> 5ade89ca6e07d5412eb0e6dccf9f63df2093969c
 </div>
 
 <footer id="footer">
