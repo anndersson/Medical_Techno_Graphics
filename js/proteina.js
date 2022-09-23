2ee0511f215cac46f3a1b698ab1f8cc827ef0555
anndersson <78931789+anndersson@users.noreply.github.com>
2022-09-23 10:48:51 -0600

    Update proteina.js

--- js/proteina.js
+++ js/proteina.js
@@ -23,7 +23,6 @@ el cuerpo humano no es capaz de utilizar las proteínas ingeridas mediante la al
 <video controls autoplay>
 <source src="img/VID-20220923-WA0013.mp4" type="video/mp4">
 </video>
-
 <h1 class="Alimentos"> Proteinas por Alimentos </h1>
 <div class="prot">
 <img class="alimentos2" src="img/inasprote.jpg">
