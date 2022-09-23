a938e77c387fe99e4ccd89d7ebff2f5fea421539
Andersson <aonavas@scl.edu.gt>
2022-09-23 08:31:31 -0600

    realize la informacion de cada proteina y de esta manera lo actualizo

--- js/proteina.js
+++ js/proteina.js
@@ -8,7 +8,12 @@ let estructura_proteina =
     ${menu}
 </header>
 
+<h1 class="Alimentos"> Proteinas Por Alimentos </h1>
 
+<div class="prot">
+<img class="alimentos2" src="img/inasprote.jpg">
+<img class="alimentos3" src="img/inas.jpg">
+</div>
 
 <footer id="footer">
     ${footer}
