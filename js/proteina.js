43b9b338ec7dd0f4b07e84b793b3e80a26dde812
anndersson <78931789+anndersson@users.noreply.github.com>
2022-09-23 09:25:07 -0600

    Update proteina.js

--- js/proteina.js
+++ js/proteina.js
@@ -7,10 +7,8 @@ let estructura_proteina =
 <header id="header">
     ${menu}
 </header>
-<<<<<<< HEAD
 
-<h1 class="Alimentos"> Proteinas Por Alimentos </h1>
-=======
+
 <h1 class="h" >SÍNTESIS DE LAS PROTEINAS </h1>
 <div class="ande" >
 <p>El proceso conocido como síntesis de proteínas viene a estabilizar el desequilibrio ocasionado por la pérdida de proteínas celulares debida a la degradación que estas sufren al intervenir en la función para la que están destinadas. 
@@ -20,8 +18,8 @@ el cuerpo humano no es capaz de utilizar las proteínas ingeridas mediante la al
 </p>
 <img class="imagenes" src="img/image 1.png" alt=""> 
 </div>
->>>>>>> 3dc1a1b7f314b055df2b72e66558d99f9dfce6a8
 
+<h1> class="Alimentos"> Proteinas Por Alimentos </h1>
 <div class="prot">
 <img class="alimentos2" src="img/inasprote.jpg">
 <img class="alimentos3" src="img/inas.jpg">
