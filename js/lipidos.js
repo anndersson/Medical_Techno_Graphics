cdad4591e9ad602e7eabb517bb6b0f56e4a36a43
davidnatareno <ednatareno@scl.edu.gt>
2022-09-26 15:03:08 -0600

    Parrafo-lipidos

--- js/lipidos.js
+++ js/lipidos.js
@@ -11,10 +11,10 @@ let estructura_lipidos =
 
 <h1 class="h" >SÍNTESIS DE LAS LIPIDOS </h1>
 <div class="ande" >
-<p>El proceso conocido como síntesis de proteínas viene a estabilizar el desequilibrio ocasionado por la pérdida de proteínas celulares debida a la degradación que estas sufren al intervenir en la función para la que están destinadas. 
+<p>Los lípidos tienen una función única en los organismos vivos, principalmente por sus estructuras hidrofóbicas.Los lípidos sirven como moléculas para almacenamiento de energía muy compactas y eficientes, 
 <br>
-Su finalidad es permitir que el organismo forme macromoléculas que necesita para llevar a cabo sus funciones.</br>
-el cuerpo humano no es capaz de utilizar las proteínas ingeridas mediante la alimentación directamente, sino que necesita romper sus enlaces peptídicos y, a partir de los aminoácidos que contienen, crear nuevas estructuras.
+componentes esenciales de las membranas biológicas y como moléculas diversas asociadas a las membranas</br>
+que pueden tener funciones de señalización o protectoras. Esto se centra en el metabolismo de las principales clases de lípidos, y esto es, de que forma se sintetizan y degradan y la manera en que se regulan dichos procesos.
 </p>
 <img class="imagenes" src="img/image 1.png" alt=""> 
 </div>
