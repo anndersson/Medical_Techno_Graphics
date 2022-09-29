b8591ed71e6bd3eee47b42df609f0a0d295033ea
Emmanuel <jefuentes@scl.edu.gt>
2022-09-29 12:20:35 -0600

    lipidos organizados

--- js/lipidos.js
+++ js/lipidos.js
@@ -1,6 +1,8 @@
 import {menu} from "./componentes/menu.js";
 import {footer} from "./componentes/footer.js";
-
+import {sdl_lipidos} from "./lipidos/sintesis_de_los_lipidos.js";
+import {v_lipidos} from "./lipidos/video_lipidos.js";
+import {elementos_lipidos} from "./lipidos/galeria_pidos.js";
 
 let estructura_lipidos =
 `
@@ -8,37 +10,19 @@ let estructura_lipidos =
     ${menu}
 </header>
 
+<section>
+    ${sdl_lipidos}
+</section>
 
-<h1 class="h" >SÍNTESIS DE LAS LIPIDOS </h1>
-<div class="ande" >
-<p>Los lípidos tienen una función única en los organismos vivos, principalmente por sus estructuras hidrofóbicas.Los lípidos sirven como moléculas para almacenamiento de energía muy compactas y eficientes, 
-<br>
-componentes esenciales de las membranas biológicas y como moléculas diversas asociadas a las membranas</br>
-que pueden tener funciones de señalización o protectoras. Esto se centra en el metabolismo de las principales clases de lípidos, y esto es, de que forma se sintetizan y degradan y la manera en que se regulan dichos procesos.
-</p>
-<img class="imagenes" src="img/image 1.png" alt=""> 
-</div>
 
 <div class="video">
-<h1 class="texto-proteinas">¿QUE SON LOS LIPIDOS?</h1>
-<video src="img/CEA1924D-33A2-4F75-B3A1-79941BDB9DDF.mov" controls autoplay>
-</video>
+    ${v_lipidos}
 </div>
 
 <h1 class="Alimentos"> Proteinas Por Alimentos </h1>
 
-    <div class="alimentos">
-        <div class="alimento">
-            <img src="img/Ellipse 9.png" alt="">
-            <div>
-                <span>Aguacate - 1.9 de Proteína por 100g</span>
-            </div>
-        </div>
-    </div>
-
-<div class="prot">
-<img class="alimentos2" src="img/inasprote.jpg">
-<img class="alimentos3" src="img/inas.jpg">
+<div class="alimentos">
+    ${elementos_lipidos}
 </div>
 
 <footer id="footer">
